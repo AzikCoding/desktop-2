4a633c120730cd41e92d1224dc1d498cd4593520
Azamat <azikcoding@gmail.com>
2022-07-05 22:37:59 +0500

    Change all codes

--- js/script.js
+++ js/script.js
@@ -1,52 +1,15 @@
-let playMusic = document.querySelectorAll('.play-btn'),
-    similarContent = document.querySelector('.similar-musics'),
-    popularContent = document.querySelector('.popular-musics'),
-    fullTextBtn = document.querySelector('.full__text'),
-    fullText = document.querySelector('.music-text'),
-    a = document.querySelector('.full__text-close');
-console.log(a);
-playMusic.forEach((item)=>{
-   item.addEventListener('click' , function(){
-    similarContent.style.display = 'block';
-    popularContent.style.display = 'none';
-   });
-});
-function adds(){
-a.style.display ='none';
-}
-
-fullTextBtn.addEventListener('click' , function(){
-    fullText.innerHTML =`
-Yurak hafa holatimdan<br>
-Seni o'zi nega sevdim<br>
-Kulib kulib telbalarga<br>
-O'zim majnun bo'lib kettim<br>  
-<br>
-Yoningdaman go'yo yo'qday<br>
-His etishdan to'yib kettim<br>
-Qara sevgimiz boshlanmay<br>
-Tugab ketti tugab ketti<br>
-<br>
-Hali sevib sevilmagan<br>
-Men ham oddiy boladirman<br>
-Kirgandim ishq ko'chasiga<br>
-Balolarga qolib kettim<br>
-<br>
-Mensiz baxtga yetganingda<br>
-Qara sensiz qayga kettim<br>
-O'ldirdingga dengizida cho'kib kettim<br>
-Dunyoning toshlariga mehir qo'ygan bag'ritosh<br> 
-<button class="full__text-close" onclick='${a.style.display ='none'}'>o'chir</button>
-`;
-});
-a.addEventListener('click', function(){
-    // a.style.display ='flex';
-
-    fullText.innerHTML =`
-        Yurak hafa holatimdan<br>
-        Seni o'zi nega sevdim<br>
-        Kulib kulib telbalarga<br>
-        O'zim majnun bo'lib kettim<br>  
-        <br>`;
-});
+let toggle = document.querySelector('.toggle'),
+    menu = document.querySelector('.modal-category'),
+    close = document.querySelector('.menu-close');
+    console.log('toggle' , toggle );
+    console.log( 'menu', menu);
+    console.log( 'close', close);
+    toggle.addEventListener('click' , function(){
+        menu.style.left = '0';
+        console.log(1)
+    })
+    menu.addEventListener('click' , function(){
+        menu.style.left = '-100%';
+        console.log(1)
+    })
 
