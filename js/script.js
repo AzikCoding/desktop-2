3c25ca940aedcd64a7dbba50dcb57282cdea864a
Azamat <azikcoding@gmail.com>
2022-07-07 06:44:48 +0500

    bugs verified

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
+let playBtn = document.querySelectorAll('.play-btn'),
+    similarContent =document.querySelector('.similar-musics') ,
+    popularContent =document.querySelector('.popular-musics') ;
+playBtn.forEach(function(item){
+    item.addEventListener('click' , function(){
+        similarContent.style.display='flex';
+        popularContent.style.display='none';
+    })
+})
+
+
 let toggle = document.querySelector('.toggle'),
     menu = document.querySelector('.modal-category'),
     close = document.querySelector('.menu-close');
-    console.log('toggle' , toggle );
-    console.log( 'menu', menu);
-    console.log( 'close', close);
+
     toggle.addEventListener('click' , function(){
         menu.style.marginLeft = '0';
-        console.log(1)
+        document.body.style.overflow ='hidden';
     })
     menu.addEventListener('click' , function(){
+        document.body.style.overflow ='';
         menu.style.marginLeft = '-100%';
-        console.log(1)
     })
 
+
+    
